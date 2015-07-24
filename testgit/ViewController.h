cf16ed38988eb1cc34d533a8c3e1639bacfabaa4
麻生 拓弥 <MILANISTA@Milanista-rMBP.local>
2015-07-25 03:37:06 +0900

    Initial Commit

---
+++ testgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgit
+//
+//  Created by 麻生 拓弥 on 2015/07/25.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testgit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testgit
+//
+//  Created by 麻生 拓弥 on 2015/07/25.
+//  Copyright (c) 2015年 麻生 拓弥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
